9a14867536507630079beef73e637ae6801218fb
CarlosMInfante <83223613+CarlosMInfante@users.noreply.github.com>
2023-08-30 13:15:47 -0400

    Completed Day 1

---
+++ Day 1/main.py
@@ -0,0 +1,32 @@
+# print('Hello World!\nHello World!')
+# print("print('what to print')")
+# print("Hello" + " " + "Carlos")
+
+# print('Day 1 - String Manipulation')
+# print('String Concatenation is done with "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print('New lines can be created with a backslash and n.')
+
+# name = input("What is your name?")
+
+# input() will get use input in console
+# The print() will print the word "Hello" and the user input
+# print('Hello ' + input("What is your name?"))
+
+# name = input("what is your name?")
+# print(len(name))
+
+# a = input('a:')
+# b = input('b:')
+
+# newA = b
+# b = a
+# a = newA
+
+# print("a = " + a)
+# print("b = " + b)
+
+print('Welcome to the Band Name Generator.')
+city_name = input("What is the name of the city you grew up in?\n")
+pet_name = input("What is the name of your pet?\n")
+print("Your band name could be " + city_name + " " + pet_name + ".")
