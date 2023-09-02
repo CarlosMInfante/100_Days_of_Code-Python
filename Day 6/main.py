58a0cf3b8bfea5f911b72b146e02ed0c4144c263
CarlosMInfante <83223613+CarlosMInfante@users.noreply.github.com>
2023-09-02 13:18:01 -0400

    Completed Day 6

---
+++ Day 6/main.py
@@ -0,0 +1,76 @@
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+my_function()
+
+# These functions were part of the website game.
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# while not at_goal():
+#     jump()
+
+"""
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    elif wall_in_front():
+        turn_left()
+    else:
+        move()
+"""
+
+
+# This was the instructors code for this one.
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     turn_right
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+
+# while not at_goal()
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
